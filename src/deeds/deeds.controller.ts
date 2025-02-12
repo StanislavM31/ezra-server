6f2222707eaec5c2f5aa1102e07d8a6b41f183d3
Stanislav Minko <minkostas@gmail.com>
2025-02-12 20:32:31 +0300

    delete method in controller

--- src/deeds/deeds.controller.ts
+++ src/deeds/deeds.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { DeedsService } from './deeds.service';
 import { GoodDeed } from './deeds.service';
 import { GoodDeedDto } from 'src/dto/deed.dto';
@@ -26,6 +26,14 @@ export class DeedsController {
 
   @Patch(':id')
   makeDeedDone(@Param('id') id: number) {
+    try {
+      return this.deedsService.makeDeedIsDone(id)
+    } catch (error) {
+      return (error as Error).message;
+    }
+  }
+  @Delete(':id')
+  removeDeed(@Param('id') id: number) {
     try {
       return this.deedsService.makeDeedIsDone(id)
     } catch (error) {
