e3806c83f2cc4b49f9d21e7aaa3ce2eb43807513
Stanislav Minko <minkostas@gmail.com>
2025-06-24 18:13:18 +0300

    new

--- src/deeds/deeds.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DeedsService } from './deeds.service';
-import { DeedsController } from './deeds.controller';
-
-@Module({
-  controllers: [DeedsController],
-  providers: [DeedsService],
-})
-export class DeedsModule {}
